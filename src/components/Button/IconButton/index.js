8eabfe27220419e87d389027f68ebc71478fbea2
dhruv.maurya <dhruv.maurya@bigohtech.com>
2023-06-25 20:01:27 +0530

    assets, folder structure, routes

---
+++ src/components/Button/IconButton/index.js
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const IconButton = () => {
+  return <Box>IconButton</Box>;
+};
+
+export default IconButton;
---
+++ src/components/Button/SimpleButton/index.js
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const SimpleButton = () => {
+  return <Box>SimpleButton</Box>;
+};
+
+export default SimpleButton;
